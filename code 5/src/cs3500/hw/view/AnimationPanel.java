cc4c8e654b0063e11d29cb4f2a648133bd3b2313
witnik <david.litianwei@gmail.com>
2017-11-02 20:19:42 -0400

    ede

---
+++ code 5/src/cs3500/hw/view/AnimationPanel.java
@@ -0,0 +1,92 @@
+package view;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+import model.IAnimation;
+import model.Move;
+import model.Oval;
+import model.Scale;
+import model.Shape;
+
+public class AnimationPanel extends JPanel implements ActionListener {
+  ArrayList<Shape> shapes = new ArrayList<Shape>();
+  ArrayList<IAnimation> moves = new ArrayList<IAnimation>();
+  int end = 0;
+  int tick = 1;
+  Timer timer = new Timer(1000, (ActionListener) this);
+  int currentTime = 0;
+  public AnimationPanel(ArrayList<Shape> shapes, ArrayList<IAnimation> moves, int tick) {
+    super();
+    this.shapes = shapes;
+    this.moves = moves;
+    this.tick = tick;
+    if(!shapes.isEmpty()) {
+      end = shapes.get(shapes.size() - 1).getDisappears();
+    }
+  }
+
+  public void draw() {
+    timer.start();
+  }
+
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    for(Shape s : shapes) {
+      int x = (int)s.getX();
+      int y = (int)s.getY();
+      int width = (int)s.getWidth();
+      int height = (int)s.getHeight();
+      Float red = s.getRed();
+      Float green = s.getGreen();
+      Float blue = s.getBlue();
+      for(IAnimation a: moves) {
+        Shape temp = a.getShape();
+        if(s.getName().equals(temp.getName())
+                && currentTime <= a.getStart()
+                && currentTime >= a.getEnd()){
+          if(a instanceof Move){
+            x += (currentTime - a.getStart()) * a.getChange().get(0);
+            y += (currentTime - a.getStart()) * a.getChange().get(1);
+          }
+          else if(a instanceof Scale) {
+            width += (currentTime - a.getStart()) * a.getChange().get(0);
+            height += (currentTime - a.getStart()) * a.getChange().get(1);
+          }
+          else {
+            red += (currentTime - a.getStart()) * a.getChange().get(0);
+            green += (currentTime - a.getStart()) * a.getChange().get(1);
+            blue += (currentTime - a.getStart()) * a.getChange().get(2);
+          }
+        }
+      }
+      if(currentTime <= s.getDisappears()
+              && currentTime >= s.getAppears()){
+        g.setColor(Color.BLACK);
+        g.fillRect(x, y, width, height);
+      }
+      if(currentTime <= s.getDisappears()
+              && currentTime >= s.getAppears()
+              && s instanceof Oval) {
+        g.setColor(Color.BLACK);
+        g.fillOval(x, y, width, height);
+      }
+    }
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (currentTime < end) {
+    currentTime++;
+    repaint();
+    }
+    else {
+      timer.stop();
+    }
+  }
+}
---
+++ code 5/src/cs3500/hw/view/VisualView.java
@@ -0,0 +1,62 @@
+package view;
+
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+import model.AnimationModel;
+import model.IAnimation;
+import model.IAnimationModel;
+import model.Rectangle;
+import model.Shape;
+
+public class VisualView extends JFrame implements IView{
+  IAnimationModel model = new AnimationModel();
+  private JScrollPane pane;
+  private JPanel panel;
+  private AnimationPanel animationPanel;
+
+  public VisualView(IAnimationModel model){
+    super();
+    this.setSize(500,500);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.model = model;
+    ArrayList<Shape> shapes = this.model.getShapes();
+    ArrayList<IAnimation> animations = this.model.getAnimation();
+    animationPanel = new AnimationPanel(shapes, model.getAnimation(), 1);
+    animationPanel.draw();
+    JScrollPane p = new JScrollPane(animationPanel);
+    this.add(p);
+    pack();
+  }
+
+  public static void main(String[] args) {
+    AnimationModel model = new AnimationModel();
+    Shape s1 = new Rectangle("r1", 20.0f, 20.0f, 10.0f, 10.0f, 8.0f, 105.0f, 126.0f, 3, 20);
+    model.addShape(s1);
+    model.move(s1, 50.0f, 60.0f, 5, 12);
+    model.animate();
+    VisualView visual = new VisualView(model);
+    visual.makeVisible();
+  }
+
+  @Override
+  public void makeVisible() {
+    this.setVisible(true);
+  }
+
+  @Override
+  public void setDisplaySpeed(int tickPerSecond) {
+
+  }
+
+  @Override
+  public void showError(String error) {
+
+  }
+
+  @Override
+  public void refresh() {
+
+  }
+}
