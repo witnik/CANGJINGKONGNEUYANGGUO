9a3b43bdb0c27d6e8f73a14321114377a6d81a96
TomPeng19971208 <peng.zi@husky.neu.edu>
2017-11-04 16:53:42 -0400

    Add files via upload

--- EasyAnimator.java
+++ EasyAnimator.java
@@ -1,5 +1,9 @@
+package view;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import cs3500.hw05.model.AnimationModel;
@@ -8,42 +12,64 @@ import cs3500.hw05.model.AnimationModel;
  * this class represent an animator, which is used to run an animation.
  * main method will take in filename, viewType, tickpersecond and outputFile.
  * if tickPersecond or outputFile is not found, formerone will be set to 1 and, output will be set to System.out.
- * throw exception if viewType or filename is not specified or valid. 
+ * throw exception if viewType or filename is not specified or valid.
  */
 public final class EasyAnimator {
 
   public static void main(String[] args) {
     // FILL IN HERE
-    String fileName= "/Users/TomPeng/Desktop/code-20/buildings.txt";
-    String viewType="text";
-    String outPutFile="/Users/TomPeng/Desktop/code-20/name.svg";
-    //String outPutFile="System.out";
-    int tickPersecond =10;
+    String fileName="";
+    String viewType="";
+    String outPutFile="System.out";
+    int tickPersecond =1;
+    IView view = new TextualView("",1);
 
-    for (int i=0; i<args.length;i++) {
-      String cmd = args[i];
-      Scanner scan = new Scanner(cmd);
-      switch(scan.next()) {
+    if (args.length%2 !=0) {view.showError("command length has to be even");
+    System.exit(-1);}
+
+    for (int i=0;i<args.length;i+=2) {
+      String s = args[i] + " "+args[i+1];
+      Scanner scan = new Scanner(s);
+      String cm = scan.next();
+      switch (cm) {
         case "-if":
-          if (scan.hasNext()) {fileName = scan.next();}
-          else {throw new IllegalArgumentException("file name not follow -if");}
+          if (scan.hasNext()) {
+            fileName = scan.next();
+          } else {
+            view.showError("file name not follow -if");
+            throw new IllegalArgumentException("file name not follow -if");
+          }
           break;
         case "-iv":
-        if (scan.hasNext()) {viewType = scan.next();}
-        else {throw new IllegalArgumentException("vie type is not entered");}
-        break;
+          if (scan.hasNext()) {
+            viewType = scan.next();
+          } else {
+            view.showError("view type is not entered");
+            throw new IllegalArgumentException("view type is not entered");
+          }
+          break;
         case "-o":
-          if (scan.hasNext()) {outPutFile = scan.next();}
+          if (scan.hasNext()) {
+            outPutFile = scan.next();
+          }
           break;
         case "-speed":
-          if (scan.hasNextInt()) {
-            int temp = scan.nextInt();
-            if (temp<1) {throw  new IllegalArgumentException("tick per second must be positive");}
-            tickPersecond = scan.nextInt();
+          if (scan.hasNext()) {
+            int temp = Integer.parseInt(scan.next());
+            if (temp < 1) {
+              view.showError("tick per second must be positive");
+              throw new IllegalArgumentException("tick per second must be positive");
+            }
+            else {
+            tickPersecond = temp;
+          }}
+          else {
+            view.showError("tick per second is not entered");
+            throw new IllegalArgumentException("tick per second is not entered");
           }
-          else {throw new IllegalArgumentException("tick per second is not entered");}
           break;
         default:
+          view.showError("Unidentified attribute");
           throw new IllegalArgumentException("Unidentified attribute");
       }
     }
@@ -55,10 +81,10 @@ public final class EasyAnimator {
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
-    IView view;
+
     if (viewType.compareTo("text")==0) {
       model.animate();
-      view = new TextualView(tickPersecond, model.toString());
+      view = new TextualView(model.toString(), tickPersecond);
       view.makeVisible();
     }
     else if (viewType.compareTo("svg")==0) {
@@ -66,7 +92,13 @@ public final class EasyAnimator {
       String message = ((SVGView)view).getSVG();
       view.makeVisible();
       try {
-        PrintWriter writer = new PrintWriter(outPutFile);
+        PrintWriter writer;
+        if (outPutFile.equalsIgnoreCase("System.out")){
+        writer = new PrintWriter(System.out);
+        }
+        else {
+          writer = new PrintWriter(outPutFile);
+        }
         writer.println(message);
         writer.close();
       } catch (FileNotFoundException e) {
@@ -78,7 +110,8 @@ public final class EasyAnimator {
       view.makeVisible();
     }
     else {
-      throw new IllegalArgumentException("unsupported view Type");
+      view.showError("Input view Type is invalid");
+      System.exit(-1);
     }
   }
 }
